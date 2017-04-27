30def5f1f27f2b71341308d70b8de1f7f175dc78
scottinet <scottinet@kuzzle.io>
2017-04-27 13:11:17 +0200

    Prevent build failure if kernel does not have PTRACER capability

--- lib/dumpme.h
+++ lib/dumpme.h
@@ -1,8 +1,8 @@
 #ifndef _DUMPME_H
-#define _DUMPME_H
+  #define _DUMPME_H
 
-#ifdef TARGET_LINUX
-#include <sys/prctl.h>
-#endif
+  #ifdef TARGET_LINUX
+    #include <sys/prctl.h>
+  #endif
 
 #endif
